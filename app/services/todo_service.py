c106ae47055b93e89813d123c9772312dfccaad2
Noah <chanwooyang@chanwooui-MacBookPro.local>
2026-01-19 17:49:21 +0900

    feat: todo_service 구현

--- app/services/todo_service.py
+++ app/services/todo_service.py
@@ -1,5 +1,15 @@
-class todo_service:
-    def get_todos(repository: todo_repository = Depends(get_todo_repository)):
-        return repository.get_all_todos()
+from sqlalchemy.orm import Session
+from app.repositories.todo_repository import TodoRepository
+from app.schemas import TodoCreate
+class TodoService:
+    def __init__(self, db: Session):
+        self.repository = TodoRepository(db)
 
+    def get_todos(self):
+        return self.repository.get_todos()
 
+    def create_todo(self, payload: TodoCreate):
+        return self.repository.create_todo(payload)
+
+    def delete_todo(self, todo_id: int):
+        return self.repository.delete_todo(todo_id)
