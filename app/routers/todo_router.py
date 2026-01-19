691f9ceaec83c57b7a796e8fced0eeb6a23cdd08
Noah <chanwooyang@chanwooui-MacBookPro.local>
2026-01-19 17:41:03 +0900

    feat: todo router 추가

--- app/routers/todo_router.py
+++ app/routers/todo_router.py
@@ -2,15 +2,27 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from app.core.db import get_db
-
+from app.services.todo_service import TodoService
+from app import schemas
 
 
 
 
 router = APIRouter(prefix = "todos", tags=["todos"])
 
-# @router.get("/")
-# async def create_todo(db: Session = Depends(get_db)):
-#     todo_service =
-#     return
+def get_todo_service(db: Session = Depends(get_db)) -> TodoService:
+    return TodoService(db)
+
+@router.post("/")
+async def create_todo(payload: schemas.TodoCreate, service: TodoService = Depends(get_todo_service)):
+    return service.create_todo(payload)
+
+@router.get("/")
+async def get_todos(service : TodoService = Depends(get_todo_service)) :
+    return service.get_todos()
+
+
+@router.delete("/{todo_id}")
+async def delete_todo(todo_id: int, service: TodoService = Depends(get_todo_service)):
+    return service.delete_todo(todo_id)
 
